aee39857b6496e64f821c3c15fe38ee25d3bd993
flaviosenne <flaviosenne123@gmail.com>
2023-01-22 09:35:52 -0300

    finalizando layout do front end

--- frontend/src/components/HabitDay.tsx
+++ frontend/src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from '@radix-ui/react-popover'
 import { ProgressBar } from './ProgressBar'
 import clsx from 'clsx'
+import { Check } from "phosphor-react";
+import * as Checkbox from '@radix-ui/react-checkbox'
 
 interface HabitProps {
     completed: number
@@ -20,7 +22,7 @@ export function HabitDay(props: HabitProps) {
                 'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
                 'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
                 'bg-violet-500 border-violet-400': completedPercentage >= 80,
-            })} 
+            })}
             />
 
             <Popover.Portal>
@@ -30,6 +32,25 @@ export function HabitDay(props: HabitProps) {
                     <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
 
                     <ProgressBar progress={completedPercentage} />
+
+                    <div className="mt-6 flex flex-col gap-3">
+                        <Checkbox.Root className='flex items-center gap-3 group'>
+
+                            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                                <Checkbox.Indicator>
+                                    <Check
+                                        size={20}
+                                        className="text-white"
+                                    />
+                                </Checkbox.Indicator>
+                            </div>
+
+                            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+                                texto
+                            </span>
+                        </Checkbox.Root>
+                    </div>
+
                     <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
                 </Popover.Content>
 
